92ea9288e2bc66b1ba599fcab99ec741c417a0df
Ian_Ramos <ianramossantos@hotmail.com>
2021-02-24 14:48:33 -0300

    Add eslint

--- src/models/User.ts
+++ src/models/User.ts
@@ -1,4 +1,5 @@
 export class User {
-  name: string
-  email: string
+  name: string;
+
+  email: string;
 }
--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,7 @@
-import express from 'express';
+import express from 'express'
 
-const app = express();
+const app = express()
 
-app.get('/', (request, response) => {
-  return response.json({ message: 'Node.js is amazing!' })
-})
+app.get('/', (request, response) => response.json({ message: 'Node.js is amazing!' }))
 
 app.listen(3333, () => console.log('Server is running on port 3333'))
